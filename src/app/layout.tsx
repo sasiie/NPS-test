3fed1101a7c9a456da86928139a36b54f6465022
sasiie <sara.radic@chasacademy.se>
2026-09-25 12:24:01 +0200

    fixade layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,11 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html
       lang="en"
